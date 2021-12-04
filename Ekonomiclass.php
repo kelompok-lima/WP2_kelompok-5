261b8544e84c8f26091b3967dee8b78707b6ab0f
kelompok-lima <94948992+kelompok-lima@users.noreply.github.com>
2021-12-04 19:57:22 +0700

    Add files via upload

---
+++ Ekonomiclass.php
@@ -0,0 +1,38 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Royal Class Executive</title>
+</head>
+    <center>
+    <br><b>Ekonomi Class</b><br><br><img src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Bus.svg" height="210" />
+    <table border="1
+    ">
+        <tr>
+            <th rowspan="2">Rute</th><br>
+        </tr>
+        <tr>
+            <th>Jadwal</th>
+            <th>Harga Tiket</th>
+        </tr>
+        <tr>
+            <td>Jakarta - Padang</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.450.000</td>
+            
+        </tr>
+        <tr>
+            <td>Jakarta - Pariaman</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.450.000</td>
+            
+        </tr>
+        <tr>
+            <td>Jakarta - Bukittinggi</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.450.000</td>
+            
+        </tr>
+    </table>
+</body>
+</center>
+</html>
---
+++ Royalclassexecutive.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html><center>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Royal Class Executive</title>
+</head>
+    <center>
+<body>
+    <br><b>Royal Class Executive</b><br><br><img src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Bus.svg" height="210" />
+    <table border="1
+    ">
+        <tr>
+            <th rowspan="2">Rute</th><br>
+        </tr>
+        <tr>
+            <th>Jadwal</th>
+            <th>Harga Tiket</th>
+        </tr>
+        <tr>
+            <td>Jakarta - Padang</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.500.000</td>
+            
+        </tr>
+        <tr>
+            <td>Jakarta - Pariaman</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.500.000</td>
+            
+        </tr>
+        <tr>
+            <td>Jakarta - Bukittinggi</td>
+            <td>10:30,<br>13:00</td>
+            <td>Rp.500.000</td>
+            
+        </tr>
+    </table>
+</body>
+</center>
+</html>
+}
